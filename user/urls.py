d619164b0a51a5b79c79d7182ec443ddfba53fa0
Anurag Bhatt <program.anurag@gmail.com>
2024-07-16 19:00:17 +0530

    chore: Register PunchRecordViewSet and TaskRecordViewSet in user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,16 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 from user.views import (
-    UserLoginApiView, UserModelViewSet
+    UserLoginApiView, UserModelViewSet,
+    PunchRecordViewSet,
+    TaskRecordViewSet
 )
 
 
 router = DefaultRouter()
 router.register('emp', UserModelViewSet)
+router.register('punch', PunchRecordViewSet)
+router.register('record', TaskRecordViewSet)
 
 urlpatterns = [
     path('login/', UserLoginApiView.as_view(), name='login'),
