87ec79c9b242361ce9f87ec60a8c506f8f95d13d
Anurag Bhatt <program.anurag@gmail.com>
2024-07-16 18:53:56 +0530

    chore: Remove unused test files and update user serializers and views for REST API integration

--- user/serializers.py
+++ user/serializers.py
@@ -2,10 +2,9 @@ from rest_framework import serializers
 from user.models import (
     User, PushRecord, TaskRecord
 )
-
+from task.models import Task
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = [
@@ -31,3 +30,45 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
             user.save()
         return user
+    
+
+class PunchRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PushRecord
+        fields = '__all__'
+        read_only_fields = ['id', 'record']
+
+class TaskRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskRecord
+        fields = '__all__'
+        read_only_fields = ['id', 'user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user.id
+        return super().create(validated_data)
+    
+    # def to_representation(self, instance):
+    #     response = super().to_representation(instance)
+    #     response['task'] = TaskSerializer(Task.objects.get(pk=response['task'])).data
+    #     response['user'] = UserSerializer(User.objects.get(pk=response['user'])).data
+    #     response['time taken'] = instance.get_total_time()
+    #     return response
+
+class TaskRecordDetailSerializer(TaskRecordSerializer):
+    class Meta:
+        model = TaskRecord
+        fields = '__all__'
+        read_only_fields = ['id', 'user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user.id
+        return super().create(validated_data)
+    
+    # def to_representation(self, instance):
+    #     response = super().to_representation(instance)
+    #     response['task'] = TaskSerializer(Task.objects.get(pk=response['task'])).data
+    #     response['user'] = UserSerializer(User.objects.get(pk=response['user'])).data
+    #     response['time taken'] = instance.get_total_time()
+    #     response['punches'] = PunchRecordSerializer(instance.get_punches(), many=True).data
+    #     return response
--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
@@ -10,7 +12,10 @@ from user.permissions import (
     UpdateOwnProfile
 )
 from user.serializers import (
-    UserSerializer
+    UserSerializer,
+    PunchRecordSerializer,
+    TaskRecordSerializer,
+    TaskRecordDetailSerializer
 )
 
 
@@ -26,3 +31,64 @@ class UserModelViewSet(viewsets.ModelViewSet):
     permission_classes = (UpdateOwnProfile, IsAuthenticated)
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'username', 'email', 'is_staff', 'is_superuser')
+
+
+class PunchRecordViewSet(viewsets.ModelViewSet):
+    serializer_class = PunchRecordSerializer
+    queryset = PushRecord.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+class TaskRecordViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskRecordSerializer
+    queryset = TaskRecord.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('task', 'user')
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return TaskRecordDetailSerializer
+        return super().get_serializer_class()
+    
+    @action(detail=True, methods=['get', 'post'])
+    def start(self, request, pk=None):
+        record = self.get_object()
+        punch = record.add_start_punch()
+        serializer = PunchRecordSerializer(punch)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=True, methods=['get', 'post'])
+    def end(self, request, pk=None):
+        record = self.get_object()
+        punch = record.add_end_punch()
+        serializer = PunchRecordSerializer(punch)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=True, methods=['get', 'post'])
+    def resume(self, request, pk=None):
+        record = self.get_object()
+        punch = record.add_resume_punch()
+        serializer = PunchRecordSerializer(punch)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=True, methods=['get', 'post'])
+    def pause(self, request, pk=None):
+        record = self.get_object()
+        punch = record.add_suspend_record()
+        serializer = PunchRecordSerializer(punch)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=True, methods=['get'])
+    def punches(self, request, pk=None):
+        record = self.get_object()
+        punches = record.get_punches()
+        serializer = PunchRecordSerializer(punches, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'])
+    def time(self, request, pk=None):
+        record = self.get_object()
+        time = record.get_total_time()
+        return Response({'time': time})
