2ee7db033172d14454a4080db60ded14018a1ca2
Anurag Bhatt <program.anurag@gmail.com>
2024-07-18 17:47:00 +0530

    chore: Update UUIDField default value in Fabricator model

--- fabricator/models.py
+++ fabricator/models.py
@@ -9,7 +9,7 @@ def contractUpload(instance, filepath):
 
 # Create your models here.
 class Fabricator(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150, unique=True, verbose_name='Fabricator Name')
     contactPerson = models.CharField(max_length=150, verbose_name='Full Name')
     contactPhone = models.CharField(max_length=13, verbose_name='Contact Number')
