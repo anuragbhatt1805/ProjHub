ca3dac34d6d6bda6c0177dc6bf1a195e6ebc04ae
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-08-05 15:21:17 +0530

    chore: Update .gitignore to include /static/designStandard

--- projhub/settings.py
+++ projhub/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_spectacular',
     'corsheaders',
-    'django_python3_ldap',
     'rest_framework',
     'rest_framework.authtoken',
     'user',
@@ -50,6 +49,7 @@ INSTALLED_APPS = [
     'project',
     'task',
     'team',
+    # 'django_python3_ldap',
 ]
 
 MIDDLEWARE = [
@@ -152,88 +152,88 @@ SPECTACULAR_SETTINGS = {
     "COMPONENT_SPLIT_REQUEST": True,
 }
 
-AUTHENTICATION_BACKENDS = [
-    "django_python3_ldap.auth.LDAPBackend",
-]
+# AUTHENTICATION_BACKENDS = [
+#     "django_python3_ldap.auth.LDAPBackend",
+# ]
 
 
 
-# The URL of the LDAP server(s).  List multiple servers for high availability ServerPool connection.
-LDAP_AUTH_URL = ["ldap://192.168.1.207:3268"]
+# # The URL of the LDAP server(s).  List multiple servers for high availability ServerPool connection.
+# LDAP_AUTH_URL = ["ldap://192.168.1.207:3268"]
 
-# Initiate TLS on connection.
-LDAP_AUTH_USE_TLS = False
+# # Initiate TLS on connection.
+# LDAP_AUTH_USE_TLS = False
 
-# Specify which TLS version to use (Python 3.10 requires TLSv1 or higher)
-import ssl
-LDAP_AUTH_TLS_VERSION = ssl.PROTOCOL_TLSv1_2
+# # Specify which TLS version to use (Python 3.10 requires TLSv1 or higher)
+# import ssl
+# LDAP_AUTH_TLS_VERSION = ssl.PROTOCOL_TLSv1_2
 
-# Specify which TLS ciphers to use.
-LDAP_AUTH_TLS_CIPHERS = "ALL"
+# # Specify which TLS ciphers to use.
+# LDAP_AUTH_TLS_CIPHERS = "ALL"
 
-# Unspecified TLS keyword arguments applied to the connection on the underlying `ldap3` library.
-LDAP_AUTH_TLS_ARGS = {}
+# # Unspecified TLS keyword arguments applied to the connection on the underlying `ldap3` library.
+# LDAP_AUTH_TLS_ARGS = {}
 
-# The LDAP search base for looking up users.
-LDAP_AUTH_SEARCH_BASE = "DC=WBTPL,DC=COM"
-# LDAP_AUTH_SEARCH_BASE = ""
+# # The LDAP search base for looking up users.
+# LDAP_AUTH_SEARCH_BASE = "ou=people,DC=WBTPL,DC=COM"
+# # LDAP_AUTH_SEARCH_BASE = ""
 
-# The LDAP class that represents a user.
-LDAP_AUTH_OBJECT_CLASS = "inetOrgPerson"
+# # The LDAP class that represents a user.
+# LDAP_AUTH_OBJECT_CLASS = "inetOrgPerson"
 
-# User model fields mapped to the LDAP
-# attributes that represent them.
-LDAP_AUTH_USER_FIELDS = {
-    "username": "uid",
-    "name": "givenName",
-    "email": "mail",
-}
+# # User model fields mapped to the LDAP
+# # attributes that represent them.
+# LDAP_AUTH_USER_FIELDS = {
+#     "username": "uid",
+#     "name": "givenName",
+#     "email": "mail",
+# }
 
-# A tuple of django model fields used to uniquely identify a user.
-LDAP_AUTH_USER_LOOKUP_FIELDS = ("username",)
+# # A tuple of django model fields used to uniquely identify a user.
+# LDAP_AUTH_USER_LOOKUP_FIELDS = ("username",)
 
-# Path to a callable that takes a dict of {model_field_name: value},
-# returning a dict of clean model data.
-# Use this to customize how data loaded from LDAP is saved to the User model.
-LDAP_AUTH_CLEAN_USER_DATA = "django_python3_ldap.utils.clean_user_data"
+# # Path to a callable that takes a dict of {model_field_name: value},
+# # returning a dict of clean model data.
+# # Use this to customize how data loaded from LDAP is saved to the User model.
+# LDAP_AUTH_CLEAN_USER_DATA = "django_python3_ldap.utils.clean_user_data"
 
-# Path to a callable that takes a user model, a dict of {ldap_field_name: [value]}
-# a LDAP connection object (to allow further lookups), and saves any additional
-# user relationships based on the LDAP data.
-# Use this to customize how data loaded from LDAP is saved to User model relations.
-# For customizing non-related User model fields, use LDAP_AUTH_CLEAN_USER_DATA.
-LDAP_AUTH_SYNC_USER_RELATIONS = "django_python3_ldap.utils.sync_user_relations"
+# # Path to a callable that takes a user model, a dict of {ldap_field_name: [value]}
+# # a LDAP connection object (to allow further lookups), and saves any additional
+# # user relationships based on the LDAP data.
+# # Use this to customize how data loaded from LDAP is saved to User model relations.
+# # For customizing non-related User model fields, use LDAP_AUTH_CLEAN_USER_DATA.
+# LDAP_AUTH_SYNC_USER_RELATIONS = "django_python3_ldap.utils.sync_user_relations"
 
-# Path to a callable that takes a dict of {ldap_field_name: value},
-# returning a list of [ldap_search_filter]. The search filters will then be AND'd
-# together when creating the final search filter.
-LDAP_AUTH_FORMAT_SEARCH_FILTERS = "django_python3_ldap.utils.format_search_filters"
+# # Path to a callable that takes a dict of {ldap_field_name: value},
+# # returning a list of [ldap_search_filter]. The search filters will then be AND'd
+# # together when creating the final search filter.
+# LDAP_AUTH_FORMAT_SEARCH_FILTERS = "django_python3_ldap.utils.format_search_filters"
 
-# Path to a callable that takes a dict of {model_field_name: value}, and returns
-# a string of the username to bind to the LDAP server.
-# Use this to support different types of LDAP server.
-LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_openldap"
+# # Path to a callable that takes a dict of {model_field_name: value}, and returns
+# # a string of the username to bind to the LDAP server.
+# # Use this to support different types of LDAP server.
+# LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_openldap"
 
-# Sets the login domain for Active Directory users.
-LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = None
+# # Sets the login domain for Active Directory users.
+# LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = None
 
-# The LDAP username and password of a user for querying the LDAP database for user
-# details. If None, then the authenticated user will be used for querying, and
-# the `ldap_sync_users`, `ldap_clean_users` commands will perform an anonymous query.
-# LDAP_AUTH_CONNECTION_USERNAME = 'wbtpl@tasksrv'
-# LDAP_AUTH_CONNECTION_PASSWORD = 'K@02md8769$!'
-LDAP_AUTH_CONNECTION_USERNAME = None
-LDAP_AUTH_CONNECTION_PASSWORD = None
+# # The LDAP username and password of a user for querying the LDAP database for user
+# # details. If None, then the authenticated user will be used for querying, and
+# # the `ldap_sync_users`, `ldap_clean_users` commands will perform an anonymous query.
+# # LDAP_AUTH_CONNECTION_USERNAME = 'wbtpl@tasksrv'
+# # LDAP_AUTH_CONNECTION_PASSWORD = 'K@02md8769$!'
+# LDAP_AUTH_CONNECTION_USERNAME = None
+# LDAP_AUTH_CONNECTION_PASSWORD = None
 
-# Use SSL on the connection.
-LDAP_AUTH_CONNECT_USE_SSL = False
+# # Use SSL on the connection.
+# LDAP_AUTH_CONNECT_USE_SSL = False
 
-# Set connection/receive timeouts (in seconds) on the underlying `ldap3` library.
-LDAP_AUTH_CONNECT_TIMEOUT = None
-LDAP_AUTH_RECEIVE_TIMEOUT = None
+# # Set connection/receive timeouts (in seconds) on the underlying `ldap3` library.
+# LDAP_AUTH_CONNECT_TIMEOUT = None
+# LDAP_AUTH_RECEIVE_TIMEOUT = None
 
-# Unspecified keyword arguments to apply to the connection in the underlying `ldap3` library.
-LDAP_AUTH_CONNECT_ARGS = {}
+# # Unspecified keyword arguments to apply to the connection in the underlying `ldap3` library.
+# LDAP_AUTH_CONNECT_ARGS = {}
 
-# Set connection pool `active` parameter on the underlying `ldap3` library.
-LDAP_AUTH_POOL_ACTIVE = True
+# # Set connection pool `active` parameter on the underlying `ldap3` library.
+# LDAP_AUTH_POOL_ACTIVE = True
